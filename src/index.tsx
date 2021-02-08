14fe59a8a9b9ac4acdca1a410776b859bc4a86bc
Bardur R. Guttesen <bardurrunason@gmail.com>
2021-02-08 02:01:57 +0100

    feat: query github api with graphql

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,42 @@
 import './index.css';
 
-import React from 'react';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import React, { FC } from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 
+const httpLink = createHttpLink({
+  uri: 'https://api.github.com/graphql',
+});
+
+const authLink = setContext((_, { headers }) => ({
+  headers: {
+    ...headers,
+    authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`,
+  },
+}));
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const Providers: FC = ({ children }) => (
+  <ApolloProvider client={client}>{children}</ApolloProvider>
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/routes/Link.tsx
+++ src/routes/Link.tsx
@@ -1,12 +1,38 @@
+import { gql, useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 
+const GET_REPOSITORY = gql`
+  query GetRepository {
+    repository(owner: "facebook", name: "react") {
+      name
+      description
+      stargazerCount
+      url
+      createdAt
+      owner {
+        avatarUrl
+        url
+      }
+    }
+  }
+`;
+
 const Link = () => {
+  const { loading, error, data } = useQuery(GET_REPOSITORY);
   const { linkId } = useParams<{ linkId: string }>();
+
+  if (error) {
+    return <p>{JSON.stringify(error)}</p>;
+  }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <p>
       Displaying link
       {linkId}
+      {JSON.stringify(data)}
     </p>
   );
 };
